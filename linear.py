6a4d77f4db0d366189d2ad2a943bef73c97c2965
Dhruv <dhruvkalra@proton.me>
2024-12-11 09:56:28 -0500

    Changes made to linear layer

--- linear.py
+++ linear.py
@@ -2,10 +2,12 @@ import numpy as np
 import math
 
 class Linear():
-    def __init__(self, in_feature, out_feature, weight_init_fn, bias_init_fn):
-        self.W = weight_init_fn(in_feature, out_feature)
-        self.b = bias_init_fn(out_feature)
+
+    def __init__(self, in_feature, out_feature):
+        self.W = np.zeros((in_feature, out_feature))
+        self.b = np.zeros((1, out_feature))
         self.feature = None
+        self.batch_size = None
 
         self.dW = np.zeros((in_feature, out_feature))
         self.db = np.zeros((1, out_feature))
@@ -17,12 +19,14 @@ class Linear():
         return self.forward(x)
 
     def forward(self, x):
+        self.batch_size = x.shape[0]
         self.feature = x
-        out = np.dot(x, self.W) + self.b
+        ones = np.ones((self.batch_size, 1))
+        out = np.dot(self.feature, self.W.T) + ones * self.b
         return out
 
     def backward(self, delta):
-        self.dW = np.dot(delta, self.feature.T)
+        self.dW = np.dot(delta.T, self.feature)
         self.db = np.mean(delta, axis=0, keepdims=True)
-        dx = np.dot(delta, self.W.T)
+        dx = np.dot(delta, self.W)
         return dx
